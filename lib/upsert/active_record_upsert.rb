a1e0dab749106eabc8b9e7decda90661ae78e859
Peter Glerup Ericson <pg.ericson@gmail.com>
2012-09-18 16:01:02 +0200

    added a upsert_hash that finds out what is unique and what is not and divides it into selector and document.
    this prevents issue #5
    https://github.com/seamusabshere/upsert/issues/5

--- lib/upsert/active_record_upsert.rb
+++ lib/upsert/active_record_upsert.rb
@@ -6,6 +6,34 @@ class Upsert
         upsert.row selector, document
       end
     end
+
+    def upsert_hash(hash)
+      hash = hash.stringify_keys
+      selector_keys = upsert_find_longest_unique_fields(hash.keys)
+      document_keys = hash.keys - selector_keys
+      upsert(hash.slice(*selector_keys), hash.slice(*document_keys))
+    end
+
+  private
+    def upsert_find_longest_unique_fields(keys)
+      found_keys = []
+      if upsert_unique_fields.present?
+        upsert_unique_fields.select do |uf|
+          uf.all?{|d| keys.include?(d)}
+        end.sort_by {|a,b| a.size <=> b.size}.last
+      else
+        keys
+      end
+    end
+
+    def upsert_unique_fields
+      unique_indexes = ActiveRecord::Base.connection.indexes(table_name).select do |index|
+        index.unique == true
+      end.map do |index|
+        index.columns
+      end
+      unique_indexes
+    end
   end
 end
 
